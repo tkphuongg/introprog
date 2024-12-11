d8db8ba302a2ee26b2a4a65b000ad1a1a2660285
Khanh Phuong Tran <k.tran@campus.tu-berlin.de>
2024-12-11 17:47:26 +0100

    sem aufg 5

--- BlockB/05_Semesteraufgaben/05ex.c
+++ BlockB/05_Semesteraufgaben/05ex.c
@@ -25,12 +25,14 @@ Aufgabe 1:
 Ändern Sie alle im Baum `t` gespeicherten Werte auf `x`.
 */
 void search_tree_boringify(TreeNode *t, uint16_t x) {
-    t->item = x;
-    if(t->left != NULL){
-        search_tree_boringify(t->left, x);
-    }
-    if(t->right != NULL){
-        search_tree_boringify(t->right, x);
+    if(t != NULL){
+        t->item = x;
+        if(t->left != NULL){
+            search_tree_boringify(t->left, x);
+        }
+        if(t->right != NULL){
+            search_tree_boringify(t->right, x);
+        }
     }
 }
 
@@ -40,6 +42,41 @@ Geben Sie die größte Zahl im Suchbaum `t` zurück, welche echt kleiner als `x`
 Falls keine solche Zahl existiert, geben Sie stattdessen `x` zurück. Die Laufzeit Ihrer Lösung soll
 proportional zur Höhe des Baumes sein, aber unabhängig von der Gesamtzahl an Knoten. 
 */
+int check_tree(TreeNode *tree_to_check, uint16_t x, uint16_t *max_lt_x){
+    if(tree_to_check == NULL){
+        return 0;
+    }
+    else if(tree_to_check->item >= x){
+        if(tree_to_check->left){
+            tree_to_check = tree_to_check->left;
+            return 1;
+        }
+        return 0;
+    }
+    else{
+        if(tree_to_check->right){
+            tree_to_check = tree_to_check->right;
+            return 1;
+        }
+        return 0;
+    }
+}
+
+void save_item_if_lt_x(TreeNode *current_node, uint16_t x, uint16_t* max_lt_x){
+    if(current_node->item < x && current_node != NULL){
+        *max_lt_x = current_node->item;
+    }
+}
+
 uint16_t search_tree_get_less_than(TreeNode *t, uint16_t x) {
-    return 0;
+    uint16_t *max_lt_x = NULL;
+    *max_lt_x = x;
+    if(t == NULL){
+        return x;
+    }
+    TreeNode *current_node = t;
+    while(check_tree(current_node, x, max_lt_x)){
+        save_item_if_lt_x(current_node, x, max_lt_x);
+    }
+    return *max_lt_x;
 }
--- BlockB/05_Semesteraufgaben/search_tree.h
+++ BlockB/05_Semesteraufgaben/search_tree.h
@@ -7,7 +7,7 @@ Ein Knoten in einem Suchbaum.
 
 Wir repräsentieren Suchbäume durch pointer auf `Nodes` (der
 leere Baum wird durch einen null-pointer repräsentiert).
-*/
+*/ 
 typedef struct TreeNode_ {
     uint16_t item; /* Der Wert, der in diesem Knoten gespeichert wird. */
     struct TreeNode_ *left; /* Der linke Teilbaum. */
